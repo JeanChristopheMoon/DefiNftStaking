10dc72ce98fb9d3d30ad2c0d4325f8862fdfdf3a
JeanChristophe <119110069+JeanChristopheMoon@users.noreply.github.com>
2024-01-13 21:32:55 +0100

    Update accounts.rs
    Adding Basic Accounts

--- accounts.rs
+++ accounts.rs
@@ -1,1 +1,22 @@
+//Accounts 
 
+//StakingPool State
+pub struct StakingPool {
+  pub owner :  Pubkey,
+  pub name: String,
+  pub totalStaked: u64,
+}
+
+//User State
+pub struct user {
+pub owner: Pubkey,
+pub name: String,
+pub amount: u64,
+}
+
+//User Dashboard State
+pub struct Dashboard {
+pub owner: Pubkey,
+pub name: String,
+pub total_staked: u64,
+}
