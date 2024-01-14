1353907051be9d2f208b986fa18187d01ce40cda
JeanChristophe <119110069+JeanChristopheMoon@users.noreply.github.com>
2024-01-14 14:43:37 +0100

    uspdating structs based on parameters

--- lib.rs
+++ lib.rs
@@ -2,11 +2,11 @@
 pub mod simple_defi {
     use super::*;
 
-pub fn initializing(ctx: Context<initilize>){
+pub fn initializing(ctx: Context<initilize>,usernumbers: u64,totalStaked: u64){
 pub initilizngstake: &mut StakingPool = &mut ctx.accounts.staking_pool;
 
-staking_pool.usernumbers = 0;
-staking_pool.totalStaked = 0;
+staking_pool.usernumbers = usernumbers;
+staking_pool.totalStaked = totalStaked;
 
 
 }
