f94d665f7860752759ec59f8bcbd15d749469aeb
JeanChristophe <119110069+JeanChristopheMoon@users.noreply.github.com>
2024-01-20 21:25:31 +0100

    Update lib.rs
    Adding return type/Result/program account within context and staking_pool

--- lib.rs
+++ lib.rs
@@ -6,40 +6,43 @@ declare_id!("x");
 mod simple_defi {
     use super::*;        
 
-    pub fn initialize(ctx: Context<Initializer>, usernumbers: u64, totalstaked: u64) {
+    pub fn initialize(ctx: Context<Initializer>, usernumbers: u64, totalstaked: u64) -> Result<()> {
         //let initializing_stake: &mut System = &mut ctx.accounts.staking_pool;
-        let initializing_stake: &mut System = &mut ctx.accounts.system_program;
+        let initializing_stake: &mut Staking = &mut ctx.accounts.staking_pool;
 
 
         initializing_stake.usernumbers = usernumbers;
         initializing_stake.totalstaked = totalstaked;
+        
+        Ok(())
     }
 
     // Accounts
 
     // StakingPool State
-    pub struct System {
+    #[account]
+    pub struct Staking {
         pub owner: Pubkey,
         pub name: String,
         pub totalstaked: u64,
         pub usernumbers: u64,
     }
 
-    // User State
-    #[account]
-    pub struct User {
-        pub owner: Pubkey,
-        pub name: String,
-        pub amount: u64,
-    }
+    // // User State
+    // #[account]
+    // pub struct User {
+    //     pub owner: Pubkey,
+    //     pub name: String,
+    //     pub amount: u64,
+    // }
 
-    // User Dashboard State
-    #[account]
-    pub struct Dashboard {
-        pub owner: Pubkey,
-        pub name: String,
-        pub total_staked: u64,
-    }
+    // // User Dashboard State
+    // #[account]
+    // pub struct Dashboard {
+    //     pub owner: Pubkey,
+    //     pub name: String,
+    //     pub total_staked: u64,
+    // }
 
     // Contexts
     #[derive(Accounts)]
@@ -48,6 +51,8 @@ mod simple_defi {
         pub owner: Signer<'info>,
         // Corrected: Use the correct account type
         #[account(init, payer = owner, space = 8 + 180)]
+        pub staking_pool: Account<'info, Staking>,
+
         pub system_program: Program<'info, System>,
         // change Account to the Program since it gives error! Why? ( Question for Practice:D)
     }
